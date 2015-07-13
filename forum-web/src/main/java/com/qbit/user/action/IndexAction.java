5d275c51f4103df8f3603b2ec6e4d37701eb177e
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2015-07-13 23:28:58 +0100

    experimental default redirection to login page

---
+++ forum-web/src/main/java/com/qbit/user/action/IndexAction.java
@@ -0,0 +1,21 @@
+package com.qbit.user.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.log4j.Logger;
+
+;
+
+/**
+ * Created by Benek on 2015-07-13.
+ */
+public class IndexAction extends ActionSupport {
+
+    public static final Logger logger = Logger.getLogger(IndexAction.class);
+
+    // all struts logic here
+    public String execute() {
+        logger.debug("in execute");
+        return "showReportResult";
+
+    }
+}
--- forum-web/src/main/java/com/qbit/user/action/WelcomeUserAction.java
+++ forum-web/src/main/java/com/qbit/user/action/WelcomeUserAction.java
@@ -11,13 +11,21 @@ public class WelcomeUserAction extends ActionSupport {
     ForumService forumService;
 
     private String username;
-
+    private String password;
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     // all struts logic here
