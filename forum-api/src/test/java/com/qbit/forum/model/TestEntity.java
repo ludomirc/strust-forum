d5c4109cdd9ba8e55a3240d310ab2e629d2c5dd8
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2015-07-17 11:54:48 +0100

    improve test

--- forum-api/src/test/java/com/qbit/forum/model/TestEntity.java
+++ forum-api/src/test/java/com/qbit/forum/model/TestEntity.java
@@ -7,6 +7,14 @@ package com.qbit.forum.model;
 import org.testng.annotations.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class TestEntity {
 
@@ -19,37 +27,102 @@ public class TestEntity {
 
     @BeforeMethod
     private void setUpMethod() {
-        em.getTransaction().begin();
+        if (!em.getTransaction().isActive()) {
+            em.getTransaction().begin();
+        }
     }
 
     @AfterMethod
     private void shotDownMethod() {
+        if (em.getTransaction().isActive()) {
+            em.getTransaction().commit();
+        }
+    }
+
+
+    @Test()
+    public void testRoleEntity() throws NoSuchFieldException {
+        String expectedDescription = "testRoleDescription";
+        String expectedName = "testRoleName";
+        Short expectedRoeId = 10;
+
+        Role role = new Role();
+        role.setDescription(expectedDescription);
+        role.setName(expectedName);
+        role.setRoleId((short) expectedRoeId);
+
+        persistAndCommit(role);
+
+        List<Object> resultList = getListCueryByCriteria(Role.class, "roleId");
+
+        assertEquals(resultList.size(), 1, "Return list size will be equal");
+        role = (Role) resultList.get(0);
+
+        assertEquals(role.getRoleId(), (short) expectedRoeId, "RoleId will be equal");
+        assertEquals(role.getName(), expectedName, "Role name will be eagle");
+    }
+
+    private void persistAndCommit(Role role) {
+        em.persist(role);
         em.getTransaction().commit();
     }
 
-    @Test()
-    public void testPrivilegeEntity() {
 
+    @Test()
+    public void testUerEntity() throws NoSuchFieldException {
+        String expectedUsername = "testTserName";
+        String expectedPassword = "test password";
+
+        User user = new User();
+        user.setUsername(expectedUsername);
+        user.setPassword(expectedPassword);
+
+        em.persist(user);
+        em.getTransaction().commit();
+
+        List<Object> resultList = getListCueryByCriteria(User.class, "username");
+
+        assertEquals(resultList.size(), 1, "Return list size will be equel");
+        user = (User) resultList.get(0);
+
+        assertEquals(user.getUsername(), expectedUsername, "User name will be equal");
+        assertNotNull(user.getPassword(), "Password can't bee null");
+    }
+
+    @Test()
+    public void testPrivilegeEntity() throws NoSuchFieldException {
+        String expectedDescription = "test privileges";
+        Short expectedPrivilegeId = 10;
         Privilege privilege = new Privilege();
 
-        privilege.setDescription("test privileges");
+        privilege.setDescription(expectedDescription);
         privilege.setPrivilegeId((short) 10);
 
         em.persist(privilege);
+        em.getTransaction().commit();
+
+        List<Object> resultList = getListCueryByCriteria(Privilege.class, "privilegeId");
+
+        assertEquals(resultList.size(), 1, "Return list size will be equel");
+        privilege = (Privilege) resultList.get(0);
+
+        assertEquals(privilege.getDescription(), expectedDescription, "Description will be equal");
+        assertEquals(privilege.getPrivilegeId(), (short) expectedPrivilegeId, "Description id will be equal");
     }
 
-    @Test()
-    public void testUserEntity() {
-        User user = new User();
+    private List<Object> getListCueryByCriteria(Class clazz, String filedName) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Object> criteriaQuery = criteriaBuilder.createQuery();
+        Root<Privilege> from = criteriaQuery.from(clazz);
 
-        user.setPassword("testPassword");
-        user.setUsername("testUsername");
-
-        em.persist(user);
-
+        //select all records
+        CriteriaQuery<Object> select = criteriaQuery.select(from);
+        select.equals(from.get(filedName));
+        TypedQuery<Object> typedQuery = em.createQuery(select);
+        return typedQuery.getResultList();
     }
 
-    @Test()
+    /*@Test()
     public void testRoleEntity() {
         Role role = new Role();
 
@@ -58,7 +131,7 @@ public class TestEntity {
         role.setRoleId((short) 10);
 
         em.persist(role);
-    }
+    }*/
 
     @AfterClass
     public void shutDown() {
