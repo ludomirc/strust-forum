3135412b736234dcdd1fdd55c825c46326b15dc1
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2015-07-17 14:00:18 +0100

    change inheritance strategy

--- forum-api/src/main/java/com/qbit/forum/model/BaseModel.java
+++ forum-api/src/main/java/com/qbit/forum/model/BaseModel.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 /**
  * Created by Benek on 2015-07-15.
  */
-@Entity
+@MappedSuperclass
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class BaseModel implements Serializable {
 
