5e8c72f7e055eed12d143e7dbb723084100df8e1
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2015-07-25 21:53:26 +0100

    remove unused import in forum-api

--- forum-api/src/test/java/com/qbit/forum/model/TestEntity.java
+++ forum-api/src/test/java/com/qbit/forum/model/TestEntity.java
@@ -9,7 +9,6 @@ import org.testng.annotations.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
--- forum-api/src/test/java/com/qbit/forum/model/TestUserToRole.java
+++ forum-api/src/test/java/com/qbit/forum/model/TestUserToRole.java
@@ -9,7 +9,6 @@ import org.testng.annotations.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import java.util.ArrayList;
 
 /**
